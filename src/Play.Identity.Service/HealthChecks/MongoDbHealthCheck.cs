232ae1d733c6b185546d11eea8c47fd46a2f35a0
Marwane Bidamou <devr.marwane@gmail.com>
2023-03-18 23:07:03 +0100

    Added the MongoDB health check class

---
+++ src/Play.Identity.Service/HealthChecks/MongoDbHealthCheck.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MongoDB.Driver;
+
+namespace Play.Identity.Service.HealthChecks
+{
+    public class MongoDbHealthCheck : IHealthCheck
+    {
+        private readonly MongoClient _client;
+
+        public MongoDbHealthCheck(MongoClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await _client.ListDatabaseNamesAsync(cancellationToken);
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception e)
+            {
+                return HealthCheckResult.Unhealthy(exception: e);
+            }
+        }
+    }
+}
