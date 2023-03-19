7910555cb31c964e7385a4049ec88b11c3810ffa
Marwane Bidamou <devr.marwane@gmail.com>
2023-03-19 08:51:26 +0000

    Switched to generic health extensions methods

--- src/Play.Identity.Service/Program.cs
+++ src/Play.Identity.Service/Program.cs
@@ -1,15 +1,12 @@
 using GreenPipes;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
+using Play.Common.HealthChecks;
 using Play.Common.MassTransit;
 using Play.Common.Settings;
 using Play.Identity.Service.Entities;
 using Play.Identity.Service.Exceptions;
-using Play.Identity.Service.HealthChecks;
 using Play.Identity.Service.HostedServices;
 using Play.Identity.Service.Settings;
 using System.Reflection;
@@ -66,17 +63,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddHealthChecks()
-    .Add(new HealthCheckRegistration(
-            name: "MongoDB",
-            factory: serviceProvider =>
-            {
-                var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-                return new MongoDbHealthCheck(mongoClient);
-            },
-            failureStatus: HealthStatus.Unhealthy,
-            tags: new[] { "ready" },
-            timeout: TimeSpan.FromSeconds(3)
-        ));
+    .AddMongoDb();
 
 var app = builder.Build();
 
@@ -111,12 +98,5 @@ app.UseCookiePolicy(new CookiePolicyOptions
 
 app.MapControllers();
 app.MapRazorPages();
-app.MapHealthChecks("/health/ready", new HealthCheckOptions
-{
-    Predicate = (check) => check.Tags.Contains("ready")
-});
-app.MapHealthChecks("/health/live", new HealthCheckOptions
-{
-    Predicate = (check) => false
-});
+app.MapPlayEconomyHealtChecks();
 app.Run();
